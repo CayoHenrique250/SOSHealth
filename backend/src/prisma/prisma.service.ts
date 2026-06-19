d4c9564af55bbcd330bfd3a8ed7e70d624f6aed1
Cayo Henrique <cayoh250@gmail.com>
2026-06-19 17:02:53 -0300

    fix prism postgres configuration

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,14 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const dbUrl = process.env.DATABASE_URL || 'file:./dev.db';
-    const adapter = new PrismaBetterSqlite3({ url: dbUrl });
-
-    super({ adapter });
+    super();
   }
 
   async onModuleInit() {
